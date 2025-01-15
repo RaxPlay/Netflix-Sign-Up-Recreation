c4706450f615822c8b7f49f2a1a2433e409674a1
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-01-15 00:46:16 -0800

    Netflix Recreation for practice.

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.querySelector('.enter-mail')
+const password = document.querySelector('.enter-password')
+const signInBtn = document.querySelector('.sign-in-btn')
+const signWithCode = document.querySelector('.sign-in-with-code-btn')
+const forgotPassword = document.querySelector('.forgot-pswrd-btn')
+const rememberMeButton = document.querySelector('.remember-me-btn')
