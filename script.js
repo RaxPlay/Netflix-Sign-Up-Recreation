d8e5d5ff1d11b71a5954aadf7722a9824425026e
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-01-16 23:13:44 -0800

    Adding good animations and removing unnecessary JS file.

--- script.js
+++
@@ -1,7 +0,0 @@
-const email = document.querySelector('.enter-mail')
-const password = document.querySelector('.enter-password')
-const signInBtn = document.querySelector('.sign-in-btn')
-const signWithCode = document.querySelector('.sign-in-with-code-btn')
-const forgotPassword = document.querySelector('.forgot-pswrd-btn')
-const rememberMeButton = document.querySelector('.remember-me-btn')
-
