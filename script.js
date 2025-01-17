9b7198b55c59a6956dd84fd7fea24ffe484d813f
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-01-16 22:46:11 -0800

    This adds more styling

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const signInBtn = document.querySelector('.sign-in-btn')
 const signWithCode = document.querySelector('.sign-in-with-code-btn')
 const forgotPassword = document.querySelector('.forgot-pswrd-btn')
 const rememberMeButton = document.querySelector('.remember-me-btn')
+
